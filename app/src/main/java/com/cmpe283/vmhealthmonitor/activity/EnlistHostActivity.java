a1182c6bae3373029f691ff7e851d89f70c88aa9
varunrisbud <varun.risbud@gmail.com>
2015-05-02 11:05:59 -0700

    Added Host model. Created basic skeleton for consuming json from Rest service.

--- app/src/main/java/com/cmpe283/vmhealthmonitor/activity/EnlistHostActivity.java
+++ app/src/main/java/com/cmpe283/vmhealthmonitor/activity/EnlistHostActivity.java
@@ -2,13 +2,23 @@ package com.cmpe283.vmhealthmonitor.activity;
 
 import android.app.TabActivity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cmpe283.vmhealthmonitor.R;
+import com.cmpe283.vmhealthmonitor.models.Host;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
 
 public class EnlistHostActivity extends TabActivity {
 
@@ -16,33 +26,8 @@ public class EnlistHostActivity extends TabActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enlist_host);
-       /* TabHost tabHost = getTabHost();
-
-        // Gallary Tab
-        Intent intentGallery = new Intent().setClass(this, EnlistHostActivity.class);
-        TabHost.TabSpec tabSpecGallery = tabHost
-                .newTabSpec("Hosts")
-                .setIndicator("Hosts")
-                .setContent(intentGallery);
-
-        // Uploads Tab
-        Intent intentUpload = new Intent().setClass(this, EnlistVMSActivity.class);
-        TabHost.TabSpec tabSpecUploadImage = tabHost
-                .newTabSpec("VMs")
-                .setIndicator("VMs")
-                .setContent(intentUpload);
-
-        // Share Tab
-        Intent intentSharedImages = new Intent().setClass(this, HealthMonitorActivity.class);
-        TabHost.TabSpec tabSpecSharedImages = tabHost
-                .newTabSpec("Health Monitor")
-                .setIndicator("Health Monitor")
-                .setContent(intentSharedImages);
-
-        tabHost.addTab(tabSpecGallery);
-        tabHost.addTab(tabSpecUploadImage);
-        tabHost.addTab(tabSpecSharedImages);
-        tabHost.setCurrentTab(0);*/
+        HttpRequestTask requestTask = new HttpRequestTask();
+        requestTask.execute();
     }
 
 
@@ -66,5 +51,41 @@ public class EnlistHostActivity extends TabActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class HttpRequestTask extends AsyncTask<Void, Void, Host[]> {
+        @Override
+        protected Host[] doInBackground(Void... params) {
+            try {
+                final String url = "http://192.168.0.7:8080/hosts";
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                Host[] hosts = restTemplate.getForObject(url, Host[].class);
+
+                System.out.println("Hello");
+                return hosts;
+            } catch (Exception e) {
+                Log.e("MainActivity", e.getMessage(), e);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Host[] host) {
+            //TextView greetingIdText = (TextView) findViewById(R.id.id_value);
+            //TextView greetingContentText = (TextView) findViewById(R.id.content_value);
+
+            if(host != null){
+                Log.d("Host is :", host[0].getName());
+                Toast.makeText(EnlistHostActivity.this, "Host is " + host[1].getName(), Toast.LENGTH_SHORT).show();
+                Log.d("Host is :", String.valueOf(host[1].getId()));
+            }
+            else
+                Log.d("Host is null", "");
+            //greetingIdText.setText(String.valueOf(host.getId()));
+            //greetingContentText.setText(host.getName());
+        }
+
     }
 }
---
+++ app/src/main/java/com/cmpe283/vmhealthmonitor/models/Host.java
@@ -0,0 +1,25 @@
+package com.cmpe283.vmhealthmonitor.models;
+
+/**
+ * Created by Varun on 5/2/2015.
+ */
+public class Host {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
